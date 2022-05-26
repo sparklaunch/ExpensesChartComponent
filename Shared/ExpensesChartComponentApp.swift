664ef5390af77a0c7636881d7e9cd111d531e066
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-26 13:02:28 +0900

    Initial Commit

---
+++ Shared/ExpensesChartComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesChartComponentApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesChartComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
